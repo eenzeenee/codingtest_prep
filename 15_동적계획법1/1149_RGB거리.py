9a472ac61ac9d422853479d7d3118378a4484ee8
eenzeenee <lililio0170@gmail.com>
2022-03-13 22:29:42 +0900

    add file

--- 15_동적계획법1/1149_RGB거리.py
+++ 15_동적계획법1/1149_RGB거리.py
@@ -7,3 +7,10 @@ log = []
 for i in range(N):
     log.append(list(map(int, input().split())))
 
+for i in range(1, len(log)):
+    log[i][0] = min(log[i-1][1], log[i-1][2]) + log[i][0]
+    log[i][1] = min(log[i-1][0], log[i-1][2]) + log[i][1]
+    log[i][2] = min(log[i-1][0], log[i-1][1]) + log[i][2]
+
+print(min(log[-1][0], log[-1][1], log[-1][2]))
+# %%
