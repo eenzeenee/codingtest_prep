72c3d05f923833fd9c586b60c5d30a7153ed2946
eenzeenee <lililio0170@gmail.com>
2022-03-22 23:45:18 +0900

    add programmers file

--- programmers/3_힙_더맵게.py
+++ programmers/3_힙_더맵게.py
@@ -32,3 +32,38 @@ def solution(scoville, K):
     return answer
 
 # 시간 초과 여전 + 실패 발생
+
+#%%
+
+def solution(scoville, K):
+    import heapq
+    answer = 0
+    while min(scoville) < K:
+        heapq.heapify(scoville)
+        try:
+            min1 = heapq.heappop(scoville)
+            min2 = heapq.heappop(scoville)
+            heapq.heappush(scoville, min1 + min2*2)
+            
+        except IndexError:
+            return -1
+        answer+=1
+    return answer
+
+# 시간 초과 여전히 발생
+
+#%%
+
+import heapq
+def solution(scoville, K):
+    answer = 0
+    scoville.sort()                     #### sort를 먼저해서 가장 작은 수가 제일 앞에 오도록
+    while scoville[0] < K:              #### min 함수 대신 인덱스를 사용하여 가장 앞의 가장 작은 수를 찾기 (속도 개선됨!!)
+        if len(scoville) == 1:
+            return -1
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
+        heapq.heappush(scoville,min1 + min2*2)
+
+        answer+=1
+    return answer
